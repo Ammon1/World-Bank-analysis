6d61750277865b7da7c614a0f1b927c04c27d339
Ammon1 <kgrod@tlen.pl>
2018-07-15 21:59:59 +0200

    Update wb.py

--- wb.py
+++ wb.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import datetime
 import pandas as pd
 
-date = (datetime.datetime(2016, 1, 1),datetime.datetime(2016, 2, 1))
-date1 = (datetime.datetime(2017, 1, 1),datetime.datetime(2017, 2, 1))
+date = (datetime.datetime(2011, 1, 1),datetime.datetime(2011, 2, 1))
+date1 = (datetime.datetime(2012, 1, 1),datetime.datetime(2012, 2, 1))
 date2=(datetime.datetime(2012, 1, 1),datetime.datetime(2012, 2, 1))
+date3=(datetime.datetime(1960, 1, 1),datetime.datetime(1960, 2, 2))
 #set up the countries I want
 
 #set up the indicator I want (just build up the dict if you want more than one)
@@ -15,8 +16,11 @@ date2=(datetime.datetime(2012, 1, 1),datetime.datetime(2012, 2, 1))
 GNP= {'NY.GNP.PCAP.CD':'GNI per Capita'}
 GNP_2016 = wbdata.get_dataframe(GNP, country="all", data_date=date,convert_date=False)
 
-life = {'SP.DYN.LE00.IN':'life'}
+life = {'SP.DYN.LE00.IN':'life_2016'}
 life_2016 = wbdata.get_dataframe(life, country="all", data_date=date,convert_date=False)
+
+life = {'SP.DYN.LE00.IN':'life_1960'}
+life_1960 = wbdata.get_dataframe(life, country="all", data_date=date3,convert_date=False)
 
 ferility={'SP.DYN.TFRT.IN':'Total Fertility Rate'}
 fer_2016 = wbdata.get_dataframe(ferility, country="all", data_date=date,convert_date=False)
@@ -30,16 +34,17 @@ pop_2017= wbdata.get_dataframe(population2, country="all", data_date=date1,conve
 
 
 migration={'SM.POP.NETM':'Migration'}
-migration_2017=wbdata.get_dataframe(migration, country="all", data_date=date2,convert_date=False)
+migration_2012=wbdata.get_dataframe(migration, country="all", data_date=date2,convert_date=False)
 #df is "pivoted", pandas' unstack fucntion helps reshape it into something plottable
-result = pd.concat([GNP_2016, fer_2016,pop_2016,pop_2017,migration_2017,life_2016], axis=1, sort=False)
+result = pd.concat([GNP_2016, fer_2016,pop_2016,pop_2017,migration_2012,life_1960,life_2016], axis=1, sort=False)
 result1=result.dropna()
 
 result1['pop_increase']=(result1['Population_2017']-result1['Population_2016'])/result1['Population_2017']
-result1['migr/pop']=(result1['Migration'])/result1['Population_2017']
+result1['net_increase']=(result1['Population_2017']-result1['Population_2016']-result1['Migration'])/result1['Population_2017']
+result1['life_diff']=result1['life_2016']-result1['life_1960']
 
 X1=result1.iloc[:,0:2].values
-X2=result1.iloc[:,5:8].values
+X2=result1.iloc[:,7:10].values
 #remove nan
 X = np.concatenate([X1,X2],axis=1)
 X_1 = np.concatenate([X1,X2],axis=1)
@@ -63,12 +68,12 @@ result1['scale']=y_kmeans
 #gnp pop_increase
 plt.xlabel('gnp')
 plt.ylabel('pop increase')
-plt.scatter(X[y_kmeans==0,0],X[y_kmeans==0,3],s=100,c='red')
-plt.scatter(X[y_kmeans==1,0],X[y_kmeans==1,3],s=100,c='blue')
-plt.scatter(X[y_kmeans==2,0],X[y_kmeans==2,3],s=100,c='green')
-plt.scatter(X[y_kmeans==3,0],X[y_kmeans==3,3],s=100,c='black')
-plt.scatter(X[y_kmeans==4,0],X[y_kmeans==4,3],s=100,c='purple')
-plt.scatter(X[y_kmeans==5,0],X[y_kmeans==5,3],s=100,c='cyan')
+plt.scatter(X[y_kmeans==0,0],X[y_kmeans==0,2],s=100,c='red')
+plt.scatter(X[y_kmeans==1,0],X[y_kmeans==1,2],s=100,c='blue')
+plt.scatter(X[y_kmeans==2,0],X[y_kmeans==2,2],s=100,c='green')
+plt.scatter(X[y_kmeans==3,0],X[y_kmeans==3,2],s=100,c='black')
+plt.scatter(X[y_kmeans==4,0],X[y_kmeans==4,2],s=100,c='purple')
+plt.scatter(X[y_kmeans==5,0],X[y_kmeans==5,2],s=100,c='cyan')
 
 #GNP  ferity
 plt.xlabel('gnp')
@@ -80,43 +85,48 @@ plt.scatter(X[y_kmeans==3,0],X[y_kmeans==3,1],s=100,c='black')
 plt.scatter(X[y_kmeans==4,0],X[y_kmeans==4,1],s=100,c='purple')
 plt.scatter(X[y_kmeans==5,0],X[y_kmeans==5,1],s=100,c='cyan')
 
-#GNP  migration
+#GNP  net increase
 plt.xlabel('gnp')
-plt.ylabel('migration')
-plt.scatter(X[y_kmeans==0,0],X[y_kmeans==0,4],s=100,c='red')
-plt.scatter(X[y_kmeans==1,0],X[y_kmeans==1,4],s=100,c='blue')
-plt.scatter(X[y_kmeans==2,0],X[y_kmeans==2,4],s=100,c='green')
-plt.scatter(X[y_kmeans==3,0],X[y_kmeans==3,4],s=100,c='black')
-plt.scatter(X[y_kmeans==4,0],X[y_kmeans==4,4],s=100,c='purple')
-plt.scatter(X[y_kmeans==5,0],X[y_kmeans==5,4],s=100,c='cyan')
+plt.ylabel('net increase')
+plt.scatter(X[y_kmeans==0,0],X[y_kmeans==0,3],s=100,c='red')
+plt.scatter(X[y_kmeans==1,0],X[y_kmeans==1,3],s=100,c='blue')
+plt.scatter(X[y_kmeans==2,0],X[y_kmeans==2,3],s=100,c='green')
+plt.scatter(X[y_kmeans==3,0],X[y_kmeans==3,3],s=100,c='black')
+plt.scatter(X[y_kmeans==4,0],X[y_kmeans==4,3],s=100,c='purple')
+plt.scatter(X[y_kmeans==5,0],X[y_kmeans==5,3],s=100,c='cyan')
+#zero increase line
+plt.plot([0, 90000], [0, 0], 'k-', lw=2)
+#GNP of 0 increase line 
+#for Euro sphere is almost exackly 40000
+plt.plot([40000, 40000], [-0.2, 0.1], 'k-', lw=2)
 
 
 #migration  pop_increase
-plt.xlabel('migration')
+plt.xlabel('net increase')
 plt.ylabel('pop increase')
-plt.scatter(X[y_kmeans==0,4],X[y_kmeans==0,3],s=100,c='red')
-plt.scatter(X[y_kmeans==1,4],X[y_kmeans==1,3],s=100,c='blue')
-plt.scatter(X[y_kmeans==2,4],X[y_kmeans==2,3],s=100,c='green')
-plt.scatter(X[y_kmeans==3,4],X[y_kmeans==3,3],s=100,c='black')
-plt.scatter(X[y_kmeans==4,4],X[y_kmeans==4,3],s=100,c='purple')
-plt.scatter(X[y_kmeans==5,4],X[y_kmeans==5,3],s=100,c='cyan')
+plt.scatter(X[y_kmeans==0,3],X[y_kmeans==0,2],s=100,c='red')
+plt.scatter(X[y_kmeans==1,3],X[y_kmeans==1,2],s=100,c='blue')
+plt.scatter(X[y_kmeans==2,3],X[y_kmeans==2,2],s=100,c='green')
+plt.scatter(X[y_kmeans==3,3],X[y_kmeans==3,2],s=100,c='black')
+plt.scatter(X[y_kmeans==4,3],X[y_kmeans==4,2],s=100,c='purple')
+plt.scatter(X[y_kmeans==5,3],X[y_kmeans==5,2],s=100,c='cyan')
 
 #ferility pop increase
 plt.xlabel('ferility')
 plt.ylabel('pop increase')
+plt.scatter(X[y_kmeans==0,1],X[y_kmeans==0,2],s=100,c='red')
+plt.scatter(X[y_kmeans==1,1],X[y_kmeans==1,2],s=100,c='blue')
+plt.scatter(X[y_kmeans==2,1],X[y_kmeans==2,2],s=100,c='green')
+plt.scatter(X[y_kmeans==3,1],X[y_kmeans==3,2],s=100,c='black')
+plt.scatter(X[y_kmeans==4,1],X[y_kmeans==4,2],s=100,c='purple')
+plt.scatter(X[y_kmeans==5,1],X[y_kmeans==5,2],s=100,c='cyan')
+
+plt.xlabel('ferility')
+plt.ylabel('net increase')
 plt.scatter(X[y_kmeans==0,1],X[y_kmeans==0,3],s=100,c='red')
 plt.scatter(X[y_kmeans==1,1],X[y_kmeans==1,3],s=100,c='blue')
 plt.scatter(X[y_kmeans==2,1],X[y_kmeans==2,3],s=100,c='green')
 plt.scatter(X[y_kmeans==3,1],X[y_kmeans==3,3],s=100,c='black')
 plt.scatter(X[y_kmeans==4,1],X[y_kmeans==4,3],s=100,c='purple')
 plt.scatter(X[y_kmeans==5,1],X[y_kmeans==5,3],s=100,c='cyan')
-
-plt.xlabel('gnp')
-plt.ylabel('life time')
-plt.scatter(X[y_kmeans==0,0],X[y_kmeans==0,2],s=100,c='red')
-plt.scatter(X[y_kmeans==1,0],X[y_kmeans==1,2],s=100,c='blue')
-plt.scatter(X[y_kmeans==2,0],X[y_kmeans==2,2],s=100,c='green')
-plt.scatter(X[y_kmeans==3,0],X[y_kmeans==3,2],s=100,c='black')
-plt.scatter(X[y_kmeans==4,0],X[y_kmeans==4,2],s=100,c='purple')
-plt.scatter(X[y_kmeans==5,0],X[y_kmeans==5,2],s=100,c='cyan')
 
